b926ea081b27f4bc432adca3c633b52f3de8374c
Hibikine Kage <goodsmile314159@gmail.com>
2017-08-06 20:49:34 +0900

    サーバーサイドの設定を追加

---
+++ application/config/twitter.sample.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+defined('CONSUMER_KEY')        OR define('CONSUMER_KEY', '');
+defined('CONSUMER_SECRET')     OR define('CONSUMER_SECRET', '');
+defined('DEFAULT_USER_KEY')    OR define('DEFAULT_USER_KEY', '');
+defined('DEFAULT_USER_SECRET') OR define('DEFAULT_USER_SECRET', '');
