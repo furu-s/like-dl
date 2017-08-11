7d8fac9f64f037d62ee0f2bcf47ad63f374a65f4
Hibikine Kage <goodsmile314159@gmail.com>
2017-08-12 03:46:11 +0900

    :heavy_plus_sign: SASS環境の導入
    * gulp-sassを追加
    * gulpの自動ビルドにsassを追加
    * 生成されるフォルダを.gitignoreに追加

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,21 @@
 import gulp from 'gulp';
+import sass from 'gulp-sass'
 import webpack from 'gulp-webpack';
 import webpackConfig from './webpack.config.js';
 
 gulp.task('start', () => {
-    gulp.watch('public/src/**/*.*', ['webpack']);
+  gulp.watch('public/src/**/*.*', ['webpack']);
+  gulp.watch('public/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('sass', () => {
+  return gulp.src('public/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css/'));
 });
 
 gulp.task('webpack', () => {
-    return gulp.src('public/src/index.jsx')
-       .pipe(webpack(webpackConfig))
-       .pipe(gulp.dest('js/'));
+  return gulp.src('public/src/index.jsx')
+   .pipe(webpack(webpackConfig))
+   .pipe(gulp.dest('js/'));
 });
